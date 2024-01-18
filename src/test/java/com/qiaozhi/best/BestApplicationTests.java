50fe54621b52de6f750f598bd32d174d45f30dd7
pangning03 <pangning03@meituan.com>
2024-01-18 14:33:32 +0800

    DEV First Commit

---
+++ src/test/java/com/qiaozhi/best/BestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiaozhi.best;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
